fe2468f827e26d52ce9bc13d90eea7bb67dccf4a
mari-devweb <169109627+mari-devweb@users.noreply.github.com>
2024-05-11 15:09:02 -0600

    Hola mundo

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/mari-devweb.js
@@ -0,0 +1,19 @@
+// https://www.javascript.com/ 
+
+/**
+ * este es un comentario de varias lineas
+ */
+
+var mascota = "gato"
+let local = 9
+const pi = 3.14 //variable constante
+
+//variables con datos primitivos, no son un objeto y no tienen metodos
+let cadena = "Esteban"
+let edad = 27
+let hugeHex = BigInt("0x1fffffffffffff") //da un valor entero o un valor de cadena, 9007199254740991n
+let verdadero = true
+let x = undefined
+
+console.log("Hola, javascript")
+
